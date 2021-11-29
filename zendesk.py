e178f6fecf9762a566cfe1e0429874930a557531
David <hud1945@gmail.com>
2021-11-28 23:55:44 -0500

    Added comments for the functions and also made domain name into an environment variable so that the user can use any domain name

--- zendesk.py
+++ zendesk.py
@@ -2,7 +2,11 @@ import os
 import requests
 from dotenv import load_dotenv, find_dotenv
 
+# This allows us to to find the environment variables for the API Get requests 
 load_dotenv(find_dotenv())
+
+# This is the main function where all of the user interface occurs
+# It will continously run until the user quits  
 def main():
     print("Welcome to the ticket viewer!")
     while(True):
@@ -28,16 +32,20 @@ def main():
                     else: 
                         print("     Please enter a valid option")
 
+# This is the API request that will get all of the tickets and send them to a helper function to parse
+# the data to output it 
 def ListAll(): 
     api = os.getenv('API_KEY')
     user = os.getenv('USER_NAME')
+    domainName = os.getenv('DOMAIN_NAME')
     try: 
-        response = requests.get('https://zcchud1945.zendesk.com/api/v2/tickets.json', auth=(user, api))
+        response = requests.get('https://'+ domainName + '.zendesk.com/api/v2/tickets.json', auth=(user, api))
         response.raise_for_status()
     except requests.HTTPError as exception:
         print(exception)
     ParseAllJson(response.json())
 
+# This function parses the JSON response body from the get all tickets and outputs the information to screen
 def ParseAllJson(content):
         for ticket in content["tickets"]:
             id = ticket["id"]
@@ -45,16 +53,19 @@ def ParseAllJson(content):
             subject = ticket["subject"]
             print("Ticket id: " + str(id) + " type: " + str(type) + " subject: " + subject)
 
+# This function uses the id given from the user to get the specific ticket that was requested by the user
 def ListSpecific(id):
     api = os.getenv('API_KEY')
     user = os.getenv('USER_NAME')
+    domainName = os.getenv('DOMAIN_NAME')
     try:
-        req = "https://zcchud1945.zendesk.com/api/v2/tickets/" + id + ".json"
+        req = 'https://' + domainName + '.zendesk.com/api/v2/tickets/' + id + '.json'
         response = requests.get(req, auth=(user, api))
     except requests.HTTPError as exception:
         print(exception)
     ParseJson(response.json())
 
+# This parses only one ticket and displays it onto the screen
 def ParseJson(content):
     ticket = content['ticket']
     id = ticket.get('id')
@@ -64,11 +75,13 @@ def ParseJson(content):
     status = ticket.get('status')
     print("Ticket id: " + str(id) + "\ntype: " + str(type) + "\nsubject: " + str(subject) + "\ncreated at: " + created_at + "\nstatus: " + status)
 
+# This request returns the total number of tickets
 def totTickets():
     api = os.getenv('API_KEY')
     user = os.getenv('USER_NAME')
+    domainName = os.getenv('DOMAIN_NAME')
     try:
-        response = requests.get('https://zcchud1945.zendesk.com/api/v2/tickets/count.json', auth=(user, api))
+        response = requests.get('https://'+ domainName + '.zendesk.com/api/v2/tickets/count.json', auth=(user, api))
     except requests.HTTPError as exception:
         print(exception)
     Json = response.json()
